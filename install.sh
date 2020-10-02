81f877904548d3493d211d3429799d4bc4fd63fd
hermesboo <71751817+hermesboo@users.noreply.github.com>
2020-10-02 03:59:55 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+cd $(dirname $0)
+echo "Copying files..."
+cp ./steelseries-arctis-5-output-game.conf /usr/share/pulseaudio/alsa-mixer/paths/ &&
+cp ./steelseries-arctis-5-output-chat.conf /usr/share/pulseaudio/alsa-mixer/paths/
+cp ./steelseries-arctis-5-usb-audio.conf /usr/share/pulseaudio/alsa-mixer/profile-sets/ &&
+cp ./91-pulseaudio-steelseries-arctis-5.rules /lib/udev/rules.d/ &&
+echo "Done!"
+exit
